62b9e0e8c12a418993112689570588ee1415ed11
LUO ZHENLIN <58946649+luozhlin@users.noreply.github.com>
2023-04-06 01:18:39 +0800

    chapter 4.4-4.8 stack

---
+++ ParenChecker.py
@@ -0,0 +1,24 @@
+from pythonds.basic import Stack
+
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol == "(":
+            s.push(symbol)
+        elif s.isEmpty():  # When string does not end, but stack is empty.
+            balanced = False
+        else:
+            s.pop()
+
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+print(parChecker('((()))'))
+print(parChecker('(()'))
